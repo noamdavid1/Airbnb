8a34b54d8c485afee1d25c824998c89a97ecf27d
Liora <lior0590@gmail.com>
2025-04-09 14:07:03 +0300

    Add airBnb font + heart and star icons

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import styles
+import { stayPreviewSvgs } from './Svgs';
 
 export function StayPreview({ stay }) {
 
@@ -10,37 +11,59 @@ export function StayPreview({ stay }) {
         ev.preventDefault()
     }
 
-    return <article className="stay-preview">
+    return (
+        <article className="stay-preview">
 
-        <Carousel onClickThumb={onDotClick} showStatus={false} showThumbs={false} useKeyboardArrows={false}>
-            <div><img src={stay.imgUrls[0]} alt="Slide 1" /></div>
-            <div><img src={stay.imgUrls[1]} alt="Slide 2" /></div>
-            <div><img src={stay.imgUrls[2]} alt="Slide 3" /></div>
-            <div><img src={stay.imgUrls[3]} alt="Slide 4" /></div>
-            <div><img src={stay.imgUrls[4]} alt="Slide 5" /></div>
-        </Carousel>
-        {/* <img src={stay.imgUrls[0]} /> */}
-        <header>
-            <Link to={`/stay/${stay._id}`}>{stay.vendor}</Link>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"
-                aria-hidden="true" role="presentation"
-                focusable="false" 
-                style={{
-                    display: 'block',
-                    fill: 'rgba(0, 0, 0, 0.5)',
-                    height: '24px',
-                    width: '24px',
-                    stroke: 'var(--linaria-theme_palette-icon-primary-inverse)',
-                    strokeWidth: 2,
-                    overflow: 'visible',
-                  }}
-                className="">
-                <path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z" className="">
-                </path>
-            </svg> */}
-        </header>
-        <h1><span>{stay.loc.city}, {stay.loc.country}</span></h1>
-        <h2>{stay.roomType}</h2>
-        <p><span>${stay.price.toLocaleString()}</span> night</p>
-    </article>
+            <Carousel onClickThumb={onDotClick} showStatus={false} showThumbs={false} useKeyboardArrows={false}
+            // renderArrowPrev={(clickHandler, hasPrev) => {
+            //     return (
+            //       <div
+            //         className={`${
+            //           hasPrev ? "absolute" : "hidden"
+            //         } top-0 bottom-0 left-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20`}
+            //         onClick={clickHandler}
+            //       >
+            //         <div className="w-9 h-9 text-white" >{'<'}</div>
+            //         {/* <LeftIcon className="w-9 h-9 text-white" /> */}
+            //       </div>
+            //     );
+            //   }}
+            //   renderArrowNext={(clickHandler, hasNext) => {
+            //     return (
+            //       <div
+            //         className={`${
+            //           hasNext ? "absolute" : "hidden"
+            //         } top-0 bottom-0 right-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20`}
+            //         onClick={clickHandler}
+            //       >
+            //         {/* <RightIcon className="w-9 h-9 text-white" /> */}
+            //         <div className="w-9 h-9 text-white" >{'>'}</div>
+            //       </div>
+            //     );
+            //   }}
+
+            >
+                <div><img src={stay.imgUrls[0]} alt="Slide 1" /></div>
+                <div><img src={stay.imgUrls[1]} alt="Slide 2" /></div>
+                <div><img src={stay.imgUrls[2]} alt="Slide 3" /></div>
+                <div><img src={stay.imgUrls[3]} alt="Slide 4" /></div>
+                <div><img src={stay.imgUrls[4]} alt="Slide 5" /></div>
+            </Carousel>
+            {/* <img src={stay.imgUrls[0]} /> */}
+            <span className='heart-svg-container'>
+                {stayPreviewSvgs.heart}
+            </span>
+
+            <div className='preview-info'>
+                <div className='location'><span>{stay.loc.city}, {stay.loc.country}</span></div>
+                <div className='roomType gray-class'>{stay.roomType}</div>
+                <div><span>${stay.price.toLocaleString()} night</span> </div>
+
+                <div className='star-svg-container'>
+                    <span>{stayPreviewSvgs.star}</span>
+                    <span>{Number.isInteger(stay.rating)? stay.rating.toFixed(1) : stay.rating}</span>
+                </div>
+            </div>
+        </article>
+    )
 }
---
+++ src/cmps/Svgs.jsx
@@ -0,0 +1,45 @@
+export const stayPreviewSvgs = {
+    heart:
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"
+            aria-hidden="true" role="presentation"
+            focusable="false"
+            style={{
+                display: 'block',
+                fill: 'rgba(0, 0, 0, 0.5)',
+                height: '24px',
+                width: '24px',
+                stroke: 'white',
+                strokeWidth: 2,
+                overflow: 'visible',
+            }}
+            className="">
+            <path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z" className="">
+            </path>
+        </svg>
+    ,
+    star:
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            aria-hidden="true"
+            role="presentation"
+            focusable="false"
+            style={{ display: 'block', height: '12px', width: '12px', fill: 'black' }}
+        // {...props} // Spread any additional props like className, style, etc.
+        >
+            <path
+                fillRule="evenodd"
+                d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"
+            ></path>
+        </svg>
+
+    ,
+
+    arrow:
+        <svg
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 4; overflow: visible;">
+            <path fill="none" d="M20 28 8.7 16.7a1 1 0 0 1 0-1.4L20 4">
+
+            </path>
+        </svg>
+}
